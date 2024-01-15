f562c39004ece36f44540625bde10148d0d09191
Alaa2472 <alasamouly@gmail.com>
2024-01-16 01:19:34 +0200

    oreo on top

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.getElementById("contento").style.display = "none";
+    document.getElementById("content").style.display = "block";
+  }, 2000); // 3000 milliseconds (3 seconds)
+});
